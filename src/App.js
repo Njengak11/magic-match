3472a62780d3d0cbdd20c0cb4d7b3003e9a6479a
Njengak11 <njengak11@gmail.com>
2021-11-09 16:23:38 +0300

    added cards

--- src/App.js
+++ src/App.js
@@ -1,36 +1,74 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 
 const cardImages=[
-  {'src': '/img/helmet-1.pmg'},
-  {'src': '/img/potion-1.pmg'},
-  {'src': '/img/ring-1.pmg'},
-  {'src': '/img/scroll-1.pmg'},
-  {'src': '/img/shield-1.pmg'},
-  {'src': '/img/sword-1.pmg'},
+  {'src': '/img/helmet-1.png'},
+  {'src': '/img/potion-1.png'},
+  {'src': '/img/ring-1.png'},
+  {'src': '/img/scroll-1.png'},
+  {'src': '/img/shield-1.png'},
+  {'src': '/img/sword-1.png'},
 ]
 
 function App() {
-const  [cards,setCards] = useState([]);
-const [turns,setTurns]= useState(0);
+const  [cards,setCards] = useState([])
+const [turns,setTurns]= useState(0)
+const [choiceOne,setChoiceOne] = useState(null)
+const [choiceTwo,setChoiceTwo] = useState(null)
 
   // shuffle cards 
-const shuffledCards = () => {
+const shuffleCards = () => {
   const shuffledCards = [...cardImages, ...cardImages]
   .sort(() => Math.random() - 0.5)
-  .map((card) => ({...card,id:Math.random() }))
+  .map((card) => ({...card, id:Math.random() }))
 
   setCards(shuffledCards)
   setTurns(0)
+}
+
+//HANDLE choices
+const handleChoice = (card) =>{
+  choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+}
+
+// compare two selected cards
+useEffect(() => {
+if (choiceOne && choiceTwo){
+  if ( choiceOne.src === choiceTwo.src){
+    console.log('they match')
+    resetTurn()
+  }else{
+    console.log('they dont match')
+    resetTurn()
+  }
+}
+},[choiceOne,choiceTwo])
+
+//reset choice & increase turn
+const resetTurn = () => {
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns + 1)
 }
 
 console.log(cards,turns)
   return (
     <div className="App">
     <h1>Magic Match</h1>
-    <button onClick={shuffledCards}>New Game</button>
+    <button onClick={shuffleCards}>New Game</button>
+
+    <div className='card-grid'>
+      {cards.map(card => (
+        <SingleCard 
+        key={card.id} 
+        card={card}
+        handleChoice={handleChoice}
+        />
+      ))}
+    </div>
   </div>
   );
 }
