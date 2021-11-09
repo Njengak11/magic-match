ccfd6e6bf0c315cd411e351464094028565a96ef
Njengak11 <njengak11@gmail.com>
2021-11-09 16:56:38 +0300

    added card matching functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import SingleCard from './components/SingleCard';
 
 
 const cardImages=[
-  {'src': '/img/helmet-1.png'},
-  {'src': '/img/potion-1.png'},
-  {'src': '/img/ring-1.png'},
-  {'src': '/img/scroll-1.png'},
-  {'src': '/img/shield-1.png'},
-  {'src': '/img/sword-1.png'},
+  {'src': '/img/helmet-1.png',matched:false},
+  {'src': '/img/potion-1.png',matched:false},
+  {'src': '/img/ring-1.png',matched:false},
+  {'src': '/img/scroll-1.png',matched:false},
+  {'src': '/img/shield-1.png',matched:false},
+  {'src': '/img/sword-1.png',matched:false},
 ]
 
 function App() {
@@ -38,7 +38,15 @@ const handleChoice = (card) =>{
 useEffect(() => {
 if (choiceOne && choiceTwo){
   if ( choiceOne.src === choiceTwo.src){
-    console.log('they match')
+    setCards(prevCards =>{
+      return prevCards.map(card => {
+        if (card.src === choiceOne.src){
+          return{...card, matched:true}
+        } else {
+          return card
+        }
+      })
+    })
     resetTurn()
   }else{
     console.log('they dont match')
@@ -46,6 +54,8 @@ if (choiceOne && choiceTwo){
   }
 }
 },[choiceOne,choiceTwo])
+
+console.log(cards)
 
 //reset choice & increase turn
 const resetTurn = () => {
